df866c8f0c3adc3425dde88e1fe588d78dcb8a26
yormedia <46250500+yormedia@users.noreply.github.com>
2023-05-08 14:45:58 +0200

    ..

--- src/magic.ts
+++ src/magic.ts
@@ -5,7 +5,8 @@ export { MagicContainerCard } from "./cards/container/container-card";
 
 /* eslint no-console: 0 */
 console.info(
-  `%c 🪄 ${bundle.name} 🪄 ` + `%c ${bundle.version} `,
+  `%c 🪄 ${bundle.name} 🪄 %c ${bundle.version} %c is installed.`,
   "color: black; font-weight: bold; background: orange",
-  "color: orange; font-weight: bold; background: black"
+  "color: orange; font-weight: bold; background: black",
+  "color: white; font-weight: bold; background: transparent"
 );
