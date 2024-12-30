40c614a22af185ddb26e628c1244847c58a65091
Carlo Hermus <info@yormedia.eu>
2024-12-30 02:49:46 +0100

    Refactor console log formatting and remove deprecated MagicCardEditor component

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export { MagicCard } from "./card/magic-card";
 //     '@import url("https://fonts.googleapis.com/css2?family=Roboto");background-color:#FFFFFF;color:#1FBEF2;padding:3px 9px 2px 0;border-radius:0 999vh 999vh 0;border:0;font-family:"Roboto", sans-serif;margin-left:-1px;margin-bottom:18px'
 // );
 console.info(
-    `%c 🪄 %c Magic Card %c v${appInfo.version} `,
+    `%c 🪄 %c Magic Card %c v${appInfo.version}  `,
     "background-color: white;color: #fff;padding: 3px 2px 3px 3px;border-radius: 14px 0 0 14px;font-family: DejaVu Sans,Verdana,Geneva,sans-serif;text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)",
     "background-color: orange;color: white;padding: 3px 3px 3px 2px;border-radius: 0 0 0 0;font-family: DejaVu Sans,Verdana,Geneva,sans-serif;text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)",
     "background-color: black;color: white;padding: 3px 3px 3px 2px;border-radius: 0 14px 14px 0;font-family: DejaVu Sans,Verdana,Geneva,sans-serif;text-shadow: 0 1px 0 rgba(1, 1, 1, 0.3)"
